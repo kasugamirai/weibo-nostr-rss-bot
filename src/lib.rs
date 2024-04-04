9fab2c4b0ee5671b994a6cfd0045e3e7c25c19cc
xy <xyzmhx@gmail.com>
2024-04-04 23:10:27 +0900

    Refactor App struct and add run method

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
 mod uid;
 pub use uid::WeiboUid;
-mod fetch;
-pub use fetch::{fetch_messages, fetch_user_info};
 mod conf;
+mod fetch;
 mod nostr;
 pub use nostr::NotePublisher;
+use nostr_db::DbConnection;
+
+pub struct App {
+    fetcher: fetch::Fetcher,
+    db: DbConnection,
+}
+
+impl App {
+    pub fn new(fetcher: fetch::Fetcher, db: DbConnection) -> Self {
+        Self { fetcher, db }
+    }
+    async fn run() {
+        let fetcher = fetch::Fetcher::new();
+        let url = "https://rsshub.app/weibo/user/1883568433";
+        let result = fetcher.fetch_user_info(url).await;
+
+        match result {
+            Ok((title, image_url)) => {
+                let message_url = "https://rsshub.app/weibo/user/1883568433";
+                let messages = fetcher.fetch_messages(message_url).await.unwrap();
+                for message in messages {
+                    db.save_message(&message).await.unwrap();
+                }
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
+}
