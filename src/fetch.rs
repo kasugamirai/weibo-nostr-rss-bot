3fde2044ac11b7d65d3fe21d531b68adab157eab
XIAO YU <xyzmhx@gmail.com>
2024-04-05 07:56:55 +0000

    Updata Fetcher struct use uid instead if link

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,8 +1,9 @@
-use nostr_sdk::{async_utility::futures_util::future::ok, bitcoin::network::message};
 use reqwest::Error;
 use rss::Channel;
 
-pub struct Fetcher {}
+pub struct Fetcher<'a> {
+    uid: &'a str,
+}
 
 pub struct UserInfo {
     pub title: String,
@@ -15,38 +16,36 @@ pub struct Message {
     pub description: String,
 }
 
-impl Fetcher {
-    pub fn new(){
-        
+impl<'a> Fetcher<'a> {
+    pub fn new(uid: &'a str) -> Self {
+        Fetcher { uid }
     }
-    pub async fn fetch_user_info(url: &str) -> Result<UserInfo, Error> {
-        let response = reqwest::get(url).await?.bytes().await?;
+
+    pub async fn fetch_user_info(&self) -> Result<UserInfo, Error> {
+        let url = format!("https://rsshub.app/weibo/user/{}", self.uid);
+        let response = reqwest::get(&url).await?.bytes().await?;
         let channel = Channel::read_from(&response[..]).unwrap();
 
         let title = channel.title().to_string();
         let image_url = channel
             .image()
             .map(|image| image.url().to_string())
-            .unwrap_or("No image URL".to_string());
+            .unwrap_or_else(|| "No image URL".to_string());
         Ok(UserInfo { title, image_url })
     }
 
-    pub async fn fetch_messages(url: &str) -> Result<Vec<Message>, Error> {
-        let response = reqwest::get(url).await?.bytes().await?;
+    pub async fn fetch_messages(&self) -> Result<Vec<Message>, Error> {
+        let url = format!("https://rsshub.app/weibo/user/{}", self.uid);
+        let response = reqwest::get(&url).await?.bytes().await?;
         let channel = Channel::read_from(&response[..]).unwrap();
 
         let messages = channel
             .items()
             .into_iter()
-            .map(|item| {
-                let title = item.title().unwrap_or("No title").to_string();
-                let link = item.link().unwrap_or("No link").to_string();
-                let description = item.description().unwrap_or("No description").to_string();
-                Message {
-                    title,
-                    link,
-                    description,
-                }
+            .map(|item| Message {
+                title: item.title().unwrap_or("No title").to_string(),
+                link: item.link().unwrap_or("No link").to_string(),
+                description: item.description().unwrap_or("No description").to_string(),
             })
             .collect();
         Ok(messages)
@@ -59,13 +58,13 @@ mod tests {
 
     #[tokio::test]
     async fn test_fetch_user_info() {
-        let url = "https://rsshub.app/weibo/user/1883568433";
-        let result = fetch_user_info(url).await;
+        let fetcher = Fetcher::new("1883568433");
+        let result = fetcher.fetch_user_info().await;
 
         match result {
-            Ok((title, image_url)) => {
-                assert!(!title.is_empty());
-                assert!(!image_url.is_empty());
+            Ok(user_info) => {
+                assert!(!user_info.title.is_empty());
+                assert!(!user_info.image_url.is_empty());
             }
             Err(e) => {
                 panic!("Failed to fetch user info: {}", e);
@@ -75,8 +74,8 @@ mod tests {
 
     #[tokio::test]
     async fn test_fetch_messages() {
-        let url = "https://rsshub.app/weibo/user/1883568433";
-        let result = fetch_messages(url).await;
+        let fetcher = Fetcher::new("1883568433");
+        let result = fetcher.fetch_messages().await;
 
         match result {
             Ok(messages) => {
